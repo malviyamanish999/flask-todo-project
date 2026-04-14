915b181d4f11b178078b4a8ef399d54ef2ff2d94
malviyamanish999 <malviyamanish999@gmail.com>
2026-04-14 15:08:33 +0530

    Added /submittodoitem API

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 client = MongoClient("mongodb://localhost:27017/")
 db = client.todo_db
 
+
 @app.route('/submittodoitem', methods=['POST'])
 def submit():
     data = request.json
